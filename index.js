5986bf6e124e2a3fd752ecb0b4b8944fc8f8afcb
unknown <biplob2358@gmail.com>
2022-11-07 23:30:54 +0600

    created service path

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const serviceCollection = client.db("shutterUp").collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+
 app.get("/", (req, res) => {
   res.send("Shutter Up server running");
 });
