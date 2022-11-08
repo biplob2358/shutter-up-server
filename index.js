7d47c73d14e144698b8ddfebc6cd1616598b327b
unknown <biplob2358@gmail.com>
2022-11-08 20:18:25 +0600

    user add service  api created

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
+
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
   } finally {
   }
 }
